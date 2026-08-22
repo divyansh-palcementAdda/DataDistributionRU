d26b9ee7383b4bb2a34d37a2482e15d86464a136
abhishek-dollop <abhishek01624@gmail.com>
2026-08-22 17:40:26 +0530

    remove dark mood toogle

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -146,11 +146,11 @@ const Header = () => {
         </div>
 
         {/* Dark mode */}
-        <button className="icon-btn" onClick={toggleDarkMode} title="Toggle dark mode">
+        {/* <button className="icon-btn" onClick={toggleDarkMode} title="Toggle dark mode">
           <svg width="15" height="15" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
             <path d="M21 12.79A9 9 0 1111.21 3 7 7 0 0021 12.79z"/>
           </svg>
-        </button>
+        </button> */}
 
         {/* Profile */}
         <div className="dropdown" ref={profileRef}>
